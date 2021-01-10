fb2d96108a55c2cdc4b6b35f122f77c0294cecf6
Marc Freudenberg <freudenbergmarc@gmail.com>
2021-01-10 11:16:59 +0100

    Live name updating

--- src/components/NameForm.svelte
+++ src/components/NameForm.svelte
@@ -1,23 +1,23 @@
 <script>
     import Textfield from "@smui/textfield";
     import IconButton, { Icon } from "@smui/icon-button";
-    import { Label } from "@smui/common";
-
     import { client } from "../stores.js";
 
     export let socket;
 
-    function updateClient() {
+    function updateClient(client) {
         console.debug("JOIN >>> registerUpdate");
-        socket.emit("registerUpdate", $client);
+        socket.emit("registerUpdate", client);
     }
+
+    $: updateClient($client);
 </script>
 
 <Textfield
     bind:value={$client.name}
     bind:disabled={$client.nameLocked}
     label="Name" />
-<IconButton toggle bind:pressed={$client.nameLocked} on:click={updateClient}>
+<IconButton toggle bind:pressed={$client.nameLocked}>
     <Icon class="material-icons">check</Icon>
     <Icon class="material-icons" on>edit</Icon>
 </IconButton>
