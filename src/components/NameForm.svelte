3761c1bb5bbdb1bbf5d80f57482254b1944c0286
Marc Freudenberg <freudenbergmarc@gmail.com>
2021-01-17 11:24:59 +0100

    Cleanup

--- src/components/NameForm.svelte
+++ src/components/NameForm.svelte
@@ -1,36 +1,36 @@
 <script>
-    import Textfield from "@smui/textfield";
-    import Button, { Label, Icon } from "@smui/button";
-    import { client, socket, role } from "../stores.js";
+  import Textfield from "@smui/textfield";
+  import Button, { Label, Icon } from "@smui/button";
+  import { client, socket, role } from "../stores.js";
 
-    $: (() => {
-        if (typeof $client !== "undefined" && $client != null) {
-            console.debug(`${$role} >>> registerUpdate`, $client);
-            $socket.emit("registerUpdate", $client);
-        }
-    })();
-
-    $: console.log($client.nameLocked);
+  $: (() => {
+    if (typeof $client !== "undefined" && $client != null) {
+      console.debug(`${$role} >>> registerUpdate`, $client);
+      $socket.emit("registerUpdate", $client);
+    }
+  })();
 </script>
 
 <div>
-    {#if typeof $client !== 'undefined' && $client != null}
-        {#if !$client.nameLocked}
-            <Textfield
-                bind:value={$client.name}
-                bind:disabled={$client.nameLocked}
-                label="Name" />
-        {/if}
-        <Button
-            touch
-            color="secondary"
-            on:click={() => ($client.nameLocked = !$client.nameLocked)}>
-            {#if $client.nameLocked}
-                <Label>{$client.name}</Label>
-                <Icon class="material-icons">edit</Icon>
-            {:else}
-                <Icon class="material-icons" on>check</Icon>
-            {/if}
-        </Button>
+  {#if typeof $client !== "undefined" && $client != null}
+    {#if !$client.nameLocked}
+      <Textfield
+        bind:value={$client.name}
+        bind:disabled={$client.nameLocked}
+        label="Name"
+      />
     {/if}
+    <Button
+      touch
+      color="secondary"
+      on:click={() => ($client.nameLocked = !$client.nameLocked)}
+    >
+      {#if $client.nameLocked}
+        <Label>{$client.name}</Label>
+        <Icon class="material-icons">edit</Icon>
+      {:else}
+        <Icon class="material-icons" on>check</Icon>
+      {/if}
+    </Button>
+  {/if}
 </div>
